e24e0018dae613105bf3ccac04650b16e0f9790f
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2022-01-06 17:47:42 +0800

    Create Store model by sequelize-cli.

---
+++ models/store.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Store extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Store.hasMany(models.Meal);
+    }
+  };
+  Store.init({
+    name: DataTypes.STRING,
+    type: DataTypes.STRING,
+    image: DataTypes.STRING,
+    score: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Store',
+  });
+  return Store;
+};
