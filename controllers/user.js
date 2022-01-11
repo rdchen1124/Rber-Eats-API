6e037ff78fb3f404b7104414204cfa0e2172726a
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2022-01-11 17:32:48 +0800

    Remove getArticle method in useController which is a testing fake route.

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,6 @@ const saltRounds = 10;
 const jwt = require('jsonwebtoken');
 const db = require('../models');
 const User = db.User;
-const user_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjEsInVzZXJfbmFtZSI6InVzZXIifSwiZXhwIjoxNjQxODEwMjk1LCJpYXQiOjE2NDE4MDY2OTV9.rk40jwkNjpCQooLAO_Gc4bcOdKdgGxP_96kbMh8pI9E';
 const private_key = 'secret';
 const userController = {
   getAuthUser: (req, res) => {
@@ -23,7 +22,7 @@ const userController = {
               user_id: user.id,
               user_name: user.username
             };
-            const token = jwt.sign({payload, exp: Math.floor(Date.now() / 1000) + (60 * 60)}, private_key);
+            const token = jwt.sign({payload, exp: Math.floor(Date.now() / 1000) + (60 * 60 * 12)}, private_key);
             res.send({ok: 1, token});
           }else{
             res.send({err: 'password is incorrect.'});
@@ -52,22 +51,6 @@ const userController = {
     }).catch(err => {
       res.json({err: err.toString()});
     });
-  },
-  getArticle: (req, res) => { // JWT test
-    const authHeader = req.headers.authorization;
-    if(authHeader !== undefined){
-      const token = authHeader.split(' ')[1];
-      jwt.verify(token, private_key, (err, decode) => {
-        if(err){
-          res.json({err: err.toString()})
-        }else{
-          console.log('jwt:', decode);
-          res.json({ok: 1});
-        }
-      })
-    }else{
-      res.json({err: 'Auth is empty'});
-    }
   }
 }
 
